4b4767e06e04cd0120d8db613154f223afcab3b5
tharunvemula10 <tarun.vemula2445@gmail.com>
2023-11-12 19:33:53 +0530

    updated changes
    updated api
    updated cards

---
+++ src/components/News.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import image1 from './Images/pexels-patrick-case-3657154.jpg'
+
+export default class News extends Component {
+     constructor() {
+          super();
+          this.state = {
+               articles: [],
+               loading: false
+          };
+     }
+
+     async componentDidMount() {
+          let data = await fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=bf62df5e33284fd2a10663b8295b5303");
+          let parsedData = await data.json();
+          this.setState({articles: parsedData.articles})
+     }
+     render() {
+          return (
+          <div className='container my-4'>
+               <h2>Headlines for today</h2>
+               <div className="row my-4">
+                    {this.state.articles.map((element) => {
+                         return <div className="col-md-4">
+                              <NewsItem title={(element.title!==null) ? element.title.slice(0, 40) : ""} description={(element.description!==null)?element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                         </div>
+                    })}
+               </div>
+          </div>
+     )}
+}
